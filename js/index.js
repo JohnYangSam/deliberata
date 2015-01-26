b5eb67db0a03c0d11f3f3039c2bf00db626b2a2a
John YS <johnys@stanford.edu>
2015-01-26 03:49:00 -0800

    add backup small icons. Add fullpage.js, start working on fullpage.js slide

--- js/index.js
+++ js/index.js
@@ -16,8 +16,48 @@ $('.flip-container').hover(function (event) {
   $(this).toggleClass('hover');
 });
 
-/* Card flipping */
-$('.flip-container').click(function (event) {
-  /* JQuery 'this' needs to be wrapped in a JQuery syntax */
+
+/* Card clicking */
+$('.back').click(function (event) {
+  // JQuery 'this' needs to be wrapped in a JQuery syntax
+  $.fn.fullpage.moveSlideRight();
   console.log($(this).data("link"));
 });
+/*
+$('.flip-container').click(function (event) {
+  // JQuery 'this' needs to be wrapped in a JQuery syntax
+  console.log($(this).data("link"));
+  $( ".flip-container, .back, .front" ).animate({
+    "height": "50px",
+    "width": "50px",
+  });
+  $( ".detail-section" ).animate({
+    "width": "900px",
+  });
+  $(".front").delay(1).queue(function(){
+    $(this).css({"background-image":"none"});
+  });
+  $(".back").each(function(index, elem) {
+    switch(index) {
+      case 0:
+        $(elem).html('<p>H</p>');
+        break;
+      case 1:
+        $(elem).html('<p>o</p>');
+        break;
+      case 2:
+        $(elem).html('<p>m</p>');
+        break;
+      case 3:
+        $(elem).html('<p>e</p>');
+        break;
+      case 4:
+        $(elem).html('<p>.</p>');
+        break;
+      default:
+        $(elem).html('<p>.</p>');
+        break;
+    }
+  });
+});
+*/
